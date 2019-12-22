540e39b0e8730157b4b712d7cf7391303b9d8cfc
Jacek <jkozicki77@gmail.com>
2019-12-22 18:03:11 +0100

    first approach

---
+++ src/components/MenuBar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menuBar.css";
+
+export default class MenuBar extends React.Component {
+  /*
+    constructor(props) {
+        super(props)
+        //this.state {};
+      }
+    */
+
+  render() {
+    return <div className="menu">menu</div>;
+  }
+}
