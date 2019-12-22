2863447e3b0c5acc6ec845b57c87f6ff1500254a
Jacek <jkozicki77@gmail.com>
2019-12-22 19:18:00 +0100

    experimenting with props - takes attribute buttonsNames (array of names) and renders buttons with these names

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "./menuBar.css";
+import { Button } from "antd";
 
 export default class MenuBar extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.state {};
+  }
+
   /*
-    constructor(props) {
-        super(props)
-        //this.state {};
-      }
+    renderButtons(){
+        this.props.buttons.map((element) => {
+                return(
+                    <Button>element</Button>
+                )
+        });
+    }
     */
 
   render() {
-    return <div className="menu">menu</div>;
+    return (
+      <div className="menu">
+        <div className="ikona"> tu ikona codersCamp </div>
+
+        {this.props.buttonsNames.map(buttonName => (
+          <Button className="button">{buttonName}</Button>
+        ))}
+      </div>
+    );
   }
 }
