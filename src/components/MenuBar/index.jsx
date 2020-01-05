4fd4ff5e29b3271e60f769b34c3c9906d97aa64c
Jacek <jkozicki77@gmail.com>
2020-01-05 16:34:45 +0100

    Mostly working, more antdesign

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -1,31 +1,49 @@
 import React from "react";
 import "./menuBar.css";
-import { Button } from "antd";
+import { Button, Menu } from "antd";
+import SubMenu from "antd/lib/menu/SubMenu";
 
 export default class MenuBar extends React.Component {
   constructor(props) {
     super(props);
-    //this.state {};
+    //this.state
   }
 
-  /*
-    renderButtons(){
-        this.props.buttons.map((element) => {
-                return(
-                    <Button>element</Button>
-                )
-        });
-    }
-    */
+  createListOfCities(currentcity = "Wrocław") {
+    const list = [
+      "Kraków",
+      "Warszawa",
+      "Wrocław",
+      "Gdańsk",
+      "Zabrze",
+      "Szczecin",
+      "Poznań"
+    ];
+    return list.filter(element => element != currentcity);
+  }
 
   render() {
+    const list = this.createListOfCities();
     return (
       <div className="menu">
-        <div className="ikona"> tu ikona codersCamp </div>
-
-        {this.props.buttonsNames.map(buttonName => (
-          <Button className="button">{buttonName}</Button>
-        ))}
+        <div className="ikona">
+          {" "}
+          <img src="/src/components/MenuBar/cc.jpg" alt="logo"></img>{" "}
+        </div>
+        <div className="buttons">
+          <Menu mode="horizontal">
+            {this.props.children}
+            <SubMenu title="Change city">
+              <Menu.Item>{list[0]}</Menu.Item>
+              <Menu.Item>{list[1]}</Menu.Item>
+              <Menu.Item>{list[2]}</Menu.Item>
+              <Menu.Item>{list[3]}</Menu.Item>
+              <Menu.Item>{list[4]}</Menu.Item>
+              <Menu.Item>{list[5]}</Menu.Item>
+            </SubMenu>
+            <Menu.Item>Log out</Menu.Item>
+          </Menu>
+        </div>
       </div>
     );
   }
